dc1ac421e82bd6135d6d26e2cf4f7e5aef712fa1
maccari <maccari@maccari-mulo.(none)>
2014-02-05 18:24:23 +0100

    fixed random generator

--- groupMetrics.py
+++ groupMetrics.py
@@ -283,7 +283,7 @@ def greedyGroupBetweenness(dataObject, q):
                 # stats only handles integer labels, that's the reason for 
                 # dummy floatToNode[] and nodeToFloat[]
                 custDist = stats.rv_discrete(values=(Bdict.keys(), 
-                    normalizedIncrement))
+                    normalizedIncrement), name="custDist")
                 r = custDist.rvs()
                 f = floatToNode[r]
                 currentBGroup.add(f)
@@ -293,7 +293,7 @@ def greedyGroupBetweenness(dataObject, q):
                 totIncrement = sum(Cdict.values())
                 normalizedIncrement = [k/totIncrement for k in Cdict.values()]
                 custDist = stats.rv_discrete(values=(Cdict.keys(), 
-                    normalizedIncrement))
+                    normalizedIncrement), name="custDist")
 
                 r = custDist.rvs()
                 f = floatToNode[r]
